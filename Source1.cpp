7673d1945212736c093b3c5ab08704bda43e6798
Harshit Gautam <48918993+ultradluffy8@users.noreply.github.com>
2020-04-02 17:54:13 +0530

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,41 @@
+/*Maximum Sum of an array after k Negations
+              Using STL            
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ll k, n;
+	cin >> n >> k;
+	vector<int> v;
+	while (n--) {
+		ll t;
+		cin >> t;
+		v.push_back(t);
+	}
+	while (k--) {
+		*min_element(v.begin(), v.end()) = -*min_element(v.begin(), v.end());
+	}
+	ll sum = 0;
+	for (auto x : v) {
+		sum += x;
+	}
+	cout << sum;
+
+	return 0;
+}
